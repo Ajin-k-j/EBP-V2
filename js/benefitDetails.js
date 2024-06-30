e6ae3bcdaaa62c3a17fdda2d02745082770fbb17
Romy-James-P <romyjamesp@gmail.com>
2024-06-30 16:56:06 +0530

    benefit details layout

---
+++ js/benefitDetails.js
@@ -0,0 +1,19 @@
+//Initialize Quill editor
+document.addEventListener("DOMContentLoaded", function() {
+    var quill = new Quill('#editor-container', {
+      theme: 'snow'
+    });
+
+  // Get the display content button and output div
+  var displayContentBtn = document.getElementById('display-content-btn');
+  var outputDiv = document.getElementById('output');
+
+  // Add click event listener to the button
+  displayContentBtn.addEventListener('click', function() {
+    // Get the HTML content from the editor
+    var content = quill.root.innerHTML;
+    // Display the content in the output div
+    outputDiv.innerHTML = content;
+  });
+
+});
